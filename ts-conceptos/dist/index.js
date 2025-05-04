d6d8a610847f9471db2d2bcd65f3675912061d32
Nicolas Colli <collinicolas01@gmail.com>
2025-05-04 16:44:10 -0300

    typescript tipos de datos

---
+++ ts-conceptos/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+//tipos basicos
+let nombre = "Juan";
+let edad = 30;
+let estado = true;
+let numeros = [1, 2, 3, 4, 5];
+let tupla = ["nivel", 22];
+let desconocido = "cualq tipo de dato";
+let fechaAct = new Date('2025-05-04');
+let tiempoAhora = new Date();
+console.log('fecha act', fechaAct);
+console.log('tiempo:', tiempoAhora);
---
+++ ts-conceptos/src/index.ts
@@ -0,0 +1,17 @@
+//tipos basicos
+let nombre: string = "Juan";
+let edad: number = 30;
+let estado: boolean = true;
+let numeros: number[] = [1, 2, 3, 4, 5];
+let tupla: [string, number] = ["nivel", 22];
+let desconocido: any = "cualq tipo de dato";
+
+let fechaAct: Date = new Date('2025-05-04');
+
+let tiempoAhora: Date = new Date();
+
+console.log('fecha act', fechaAct);
+
+console.log('tiempo:', tiempoAhora);
+
+
