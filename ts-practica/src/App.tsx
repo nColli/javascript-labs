e968bfff41192a28c664f870bc9ce12fa52d29e3
Nicolas Colli <collinicolas01@gmail.com>
2025-06-15 10:40:36 -0300

    Typescript practica tipos

--- ts-practica/src/App.tsx
+++ ts-practica/src/App.tsx
@@ -8,6 +8,17 @@ interface Usuario {
   apellido: string;
 }
 
+//Generics: permite escribir funciones y clases que funcionan con cualq tipo
+function identidad<T>(valor: T) {
+  return valor
+}
+
+//tipos en funciones
+function mostrarUsuario(usuario: Usuario): void {
+  const {dni, nombre, apellido} = usuario
+  console.log("DNI", dni, "nombre", nombre, "apellido", apellido)
+}
+
 function App() {
   const [contador, setContador] = useState<number>(0)
 
@@ -29,6 +40,51 @@ function App() {
     nombre: "Juan",
     apellido: "Perez"
   }
+
+  const num = identidad<number>(100)
+  const text = identidad<string>("Hola mundo") 
+
+  console.log("num", num, "text", text)
+
+  //no es valido:
+  //let textoModif = identidad<string>("Hola mundo") 
+  //textoModif = 100
+
+  mostrarUsuario(newUser)
+
+  //tipos avanzados
+  //union - valor puede ser de mas de un tipo
+  let resultado: string | number
+  resultado = "diez"
+  resultado = 10
+  console.log("Resultado", resultado)
+  
+  //aliases - nombres personalizados para tipos
+  type ID = number | string
+  let userId: ID = "11.321.314"
+  userId = 10000
+
+  console.log("UserId", userId)
+
+  //tipo opcional - prop no es obligatoria
+  interface Config {
+    color?: string;
+    size: number;
+  }
+
+  const config1: Config = {
+    color: "#112233",
+    size: 100
+  }
+
+  console.log("Config 1", config1)
+
+  const config2: Config = {
+    size: 100
+  }
+
+  console.log("Config 2", config2)
+
 
   return (
     <>
